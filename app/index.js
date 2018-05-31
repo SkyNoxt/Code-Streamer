6e7ed89d078c2ff1b5faff1c4b877d57c45c1141
Sky Noxt <skynoxt@gmail.com>
2018-05-31 20:11:15 +0200

    Adding working golden-layout
    Signed-off-by: Sky Noxt <skynoxt@gmail.com>

--- app/index.js
+++ app/index.js
@@ -1,6 +1,45 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './components/App.jsx';
+const React = require ("react");
+const ReactDOM = require('react-dom');
+
+window.React = React;
+window.ReactDOM = ReactDOM;
+
+const $ = require('jquery');
+window.$ = $;
+const GoldenLayout = require('golden-layout');
+
+var myLayout = new GoldenLayout({
+	content: [{
+	  type: 'row',
+	  content:[{
+		type:'react-component',
+		component: 'test-component',
+		props: { label: 'A' }
+	  },{
+		type: 'column',
+		content:[{
+		  type:'react-component',
+		  component: 'test-component',
+		  props: { label: 'B' }
+		},{
+		  type:'react-component',
+		  component: 'test-component',
+		  props: { label: 'C' }
+		}]
+	  }]
+	}]
+  });
+ 
+class TestComponent extends React.Component {
+   render() {
+	 return null;
+   }
+ };
+ 
+ myLayout.registerComponent( 'test-component', TestComponent );
+ 
+ //Once all components are registered, call
+ myLayout.init();
 
 // Rendering
-ReactDOM.render( <App />, document.getElementById('root') );
+ReactDOM.render( <TestComponent />, document.getElementById('CodeStreamer') );
