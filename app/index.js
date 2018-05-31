c6ac44fb72b05486ecde0d2fbf0d349bf0216576
Sky Noxt <skynoxt@gmail.com>
2018-05-31 20:23:32 +0200

    Correctly initializing dependencies
    Signed-off-by: Sky Noxt <skynoxt@gmail.com>

--- app/index.js
+++ app/index.js
@@ -1,45 +1,46 @@
-const React = require ("react");
+const React = require("react");
 const ReactDOM = require('react-dom');
+
+const $ = require('jquery');
+window.$ = $;
+
+const GoldenLayout = require('golden-layout');
 
 window.React = React;
 window.ReactDOM = ReactDOM;
 
-const $ = require('jquery');
-window.$ = $;
-const GoldenLayout = require('golden-layout');
-
 var myLayout = new GoldenLayout({
 	content: [{
-	  type: 'row',
-	  content:[{
-		type:'react-component',
-		component: 'test-component',
-		props: { label: 'A' }
-	  },{
-		type: 'column',
-		content:[{
-		  type:'react-component',
-		  component: 'test-component',
-		  props: { label: 'B' }
-		},{
-		  type:'react-component',
-		  component: 'test-component',
-		  props: { label: 'C' }
+		type: 'row',
+		content: [{
+			type: 'react-component',
+			component: 'test-component',
+			props: { label: 'A' }
+		}, {
+			type: 'column',
+			content: [{
+				type: 'react-component',
+				component: 'test-component',
+				props: { label: 'B' }
+			}, {
+				type: 'react-component',
+				component: 'test-component',
+				props: { label: 'C' }
+			}]
 		}]
-	  }]
 	}]
-  });
- 
+});
+
 class TestComponent extends React.Component {
-   render() {
-	 return null;
-   }
- };
- 
- myLayout.registerComponent( 'test-component', TestComponent );
- 
- //Once all components are registered, call
- myLayout.init();
+	render() {
+		return null;
+	}
+};
+
+myLayout.registerComponent('test-component', TestComponent);
+
+//Once all components are registered, call
+myLayout.init();
 
 // Rendering
-ReactDOM.render( <TestComponent />, document.getElementById('CodeStreamer') );
+ReactDOM.render(<TestComponent />, document.getElementById('CodeStreamer'));
--- program.js
+++ program.js
@@ -11,11 +11,13 @@ const url = require('url')
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow
 
-function createWindow () {
+function createWindow() {
   // Create the browser window.
-  mainWindow = new BrowserWindow({width: 800, height: 600, webPreferences: {
-    nativeWindowOpen: true
-  }})
+  mainWindow = new BrowserWindow({
+    width: 800, height: 600, webPreferences: {
+      nativeWindowOpen: true
+    }
+  })
 
   // and load the index.html of the app.
   mainWindow.loadURL(url.format({
