51760990869b61032a443dfeb1920b5110ba7f38
Sky Noxt <skynoxt@gmail.com>
2018-06-03 17:02:26 +0200

    Cleanups and refactoring
    Signed-off-by: Sky Noxt <skynoxt@gmail.com>

---
+++ src/CodeStreamer.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import $ from "jquery";
+import GoldenLayout from "golden-layout";
+
+import "golden-layout/src/css/goldenlayout-base.css";
+import "golden-layout/src/css/goldenlayout-dark-theme.css";
+
+import Configuration from "./Configuration";
+
+window.$ = $;
+window.React = React;
+window.ReactDOM = ReactDOM;
+
+export default class CodeStreamer extends React.Component {
+	constructor(props) {
+		super(props);
+		var codeStreamer = new GoldenLayout({
+			content: [{
+				type: 'row',
+				content: [{
+					title: "Configuration",
+					type: 'react-component',
+					component: 'Configuration',
+				}]
+			}]
+		});
+
+		codeStreamer.registerComponent('Configuration', Configuration);
+		
+		this.state = { codeStreamer: codeStreamer }
+	}
+
+	componentDidMount() {
+		this.state.codeStreamer.init();
+	}
+
+	render() {
+		return null;
+	}
+}
---
+++ src/Configuration.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+
+import {
+	DiagramEngine,
+	DiagramModel,
+	DefaultNodeModel,
+	DiagramWidget
+} from "storm-react-diagrams";
+
+import "storm-react-diagrams/dist/style.min.css"
+import "./Configuration.css"
+
+export default class Configuration extends React.Component {
+	constructor(props) {
+		super(props);
+
+		var engine = new DiagramEngine();
+		engine.installDefaultFactories();
+
+		var diagram = new DiagramModel();
+		engine.setDiagramModel(diagram);
+
+		var node1 = new DefaultNodeModel("Node 1", "rgb(0,192,255)");
+		let port1 = node1.addOutPort("Out");
+		node1.setPosition(100, 100);
+
+		var node2 = new DefaultNodeModel("Node 2", "rgb(192,255,0)");
+		let port2 = node2.addInPort("In");
+		node2.setPosition(400, 100);
+
+		let link1 = port1.link(port2);
+		link1.addLabel("Hello World!");
+
+		diagram.addAll(node1, node2, link1);
+
+		this.state = { engine: engine, diagram: diagram };
+	}
+
+	render() {
+		return <DiagramWidget className="configuration" diagramEngine={this.state.engine} />
+	}
+}
