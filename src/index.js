4426adc05d81373f193e9708b750def052a7e52c
Sky Noxt <skynoxt@gmail.com>
2018-10-15 00:11:15 +0200

    Simpler way of initializing the app
    Signed-off-by: Sky Noxt <skynoxt@gmail.com>

--- src/index.js
+++ src/index.js
@@ -1,7 +1,4 @@
 
-import React from "react";
-import ReactDOM from "react-dom";
-
 import CodeStreamer from "./CodeStreamer";
 
-ReactDOM.render(<CodeStreamer />, document.getElementById('CodeStreamer'));
+new CodeStreamer();
