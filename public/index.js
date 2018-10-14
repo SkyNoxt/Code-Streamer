4c240090365bae1c35082b2a9f7cc13bdabff9b7
Sky Noxt <skynoxt@gmail.com>
2018-10-15 01:34:17 +0200

    Functional custom frame, and adapted style to match it
    Signed-off-by: Sky Noxt <skynoxt@gmail.com>

--- public/index.js
+++ public/index.js
@@ -1,4 +1,32 @@
 
 import CodeStreamer from "../src/CodeStreamer";
 
+const remote = window.require('electron').remote;
+
+function init() {
+	document.getElementById("min-btn").addEventListener("click", function (e) {
+		remote.getCurrentWindow().minimize();
+	});
+
+	document.getElementById("max-btn").addEventListener("click", function (e) {
+		const window = remote.getCurrentWindow();
+		if (!window.isMaximized()) {
+			window.maximize();
+		} else {
+			window.unmaximize();
+		}
+	});
+
+	document.getElementById("close-btn").addEventListener("click", function (e) {
+		const window = remote.getCurrentWindow().close();
+	});
+};
+
+document.onreadystatechange = function () {
+	if (document.readyState == "complete") {
+		init();
+	}
+};
+
+
 new CodeStreamer();
