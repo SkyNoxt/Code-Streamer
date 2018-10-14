6880eef7316aab46e2ab03e75f4562a77c527daa
Sky Noxt <skynoxt@gmail.com>
2018-10-14 03:47:04 +0200

    Added dat.GUI
    Signed-off-by: Sky Noxt <skynoxt@gmail.com>

--- src/Configuration/Nodes/NetworkSocket/NetworkSocket.jsx
+++ src/Configuration/Nodes/NetworkSocket/NetworkSocket.jsx
@@ -1,23 +1,37 @@
 
+import React from "react";
+import ReactDOM from "react-dom";
+
+import { GUI } from 'dat.gui';
+import "./GUI.css";
+
 import Node from "../../Node"
 
 export default class NetworkSocket extends Node {
 
 	constructor() {
-		super("Network Socket", "rgb(255, 192, 0)");
+		super("Network Socket", "rgb(255, 192, 0)", new NetworkSocketControls());
+
+		this.options = {
+			protocol: 'udp4',
+			multicastIP: "",
+			port: 1024
+		};
 
 		this.packets = this.addOutPort("Packets", (port) => this.PacketsConnected(port), (packet) => this.PacketSent(packet));
+
 		const socket = window.require('electron').remote.require("dgram").createSocket({ type: "udp4", reuseAddr: true });
 
-		socket.on("listening", function () {
-			socket.addMembership("224.111.111.111");
+		socket.on("listening", () => {
+			if (this.options.multicastIP)
+				socket.addMembership(this.options.multicastIP);
 		});
 
 		socket.on("message", (message, info) => {
 			this.packets.transmit(message);
 		});
 
-		socket.bind(22017);
+		socket.bind(this.options.port);
 	}
 
 	PacketsConnected(port) {
@@ -26,5 +40,30 @@ export default class NetworkSocket extends Node {
 
 	PacketSent(packet) {
 		console.log("Packet sent " + packet);
+	}
+}
+
+class NetworkSocketControls extends React.Component {
+
+	title = "Network Socket";
+
+	componentWillMount() {
+
+	}
+
+	componentDidMount() {
+
+		const gui = new GUI({ autoplace: false });
+
+		gui.add(this.props.node.options, 'protocol', { udp4: "udp4", udp6: "udp6" }).name("Protocol").onFinishChange((option) => { });
+		gui.add(this.props.node.options, 'multicastIP').name("Multicast IP").onFinishChange((option) => { });
+		gui.add(this.props.node.options, 'port').name("Port").onFinishChange((option) => { });
+
+		let DOMNode = ReactDOM.findDOMNode(this);
+		DOMNode.appendChild(gui.domElement);
+	}
+
+	render() {
+		return <div />;
 	}
 }
