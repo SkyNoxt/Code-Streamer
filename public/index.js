aa79c9398b4438deff7c99a6caefcac73aaceb20
Sky Noxt <skynoxt@gmail.com>
2018-10-28 20:31:04 +0100

    Added About Window
    Signed-off-by: Sky Noxt <skynoxt@gmail.com>

--- public/index.js
+++ public/index.js
@@ -1,9 +1,30 @@
 
 import CodeStreamer from "../src/CodeStreamer";
 
-var browserWindow = window.require('electron').remote.getCurrentWindow();
+const browserWindow = window.require('electron').remote.getCurrentWindow();
+const openAboutWindow = window.require('electron').remote.require("about-window").default;
 
 function init() {
+	document.getElementById("about-btn").addEventListener("click", function (e) {
+		openAboutWindow({
+			icon_path: "../../build/img/code-streamer.svg",
+			product_name: "Code Streamer",
+			//package_json_dir: "",
+			bug_report_url: "www.bitfeeling.net",
+			bug_link_text: "BitFeeling",
+			copyright: "© Copyright 2018 BitFeeling, all rights reserved.",
+			homepage: "www.bitfeeling.net",
+			description: "Flow Based Programming Framework",
+			//license: "",
+			//win_options: { },
+			//css_path: [ "" ],
+			//adjust_window_size: false,
+			open_devtools: true,
+			//use_inner_html: false,
+			use_version_info: true
+		});
+	});
+
 	document.getElementById("min-btn").addEventListener("click", function (e) {
 		browserWindow.minimize();
 	});
