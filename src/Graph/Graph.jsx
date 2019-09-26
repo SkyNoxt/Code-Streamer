56af08e9eafb0cd6881f3701836c9d1da888c712
Sky Noxt <skynoxt@gmail.com>
2019-09-26 18:32:47 +0200

    Added basic component tray with drag and drop functionality of dynamically loaded components
    Signed-off-by: Sky Noxt <skynoxt@gmail.com>

--- src/Graph/Graph.jsx
+++ src/Graph/Graph.jsx
@@ -11,25 +11,71 @@ export default class Graph extends React.Component {
     constructor(props) {
         super(props);
 
+        this.models = [];
+
         import("./Plugins/NetworkSocket").then(mod => {
             for (const modul of mod.default)
-                model.addAll(new modul());
-
-            this.forceUpdate();
+                this.models.push(modul);
         });
 
         this.engine = new DiagramEngine();
         this.engine.installDefaultFactories();
         this.engine.registerNodeFactory(new PluginFactory());
 
-        let model = new DiagramModel();
-
-        this.engine.setDiagramModel(model);
+        this.engine.setDiagramModel(new DiagramModel());
     }
 
     render() {
         return (
-            <Diagram className="graph" diagramEngine={this.engine} />
+            <div className="graph"
+                onDrop={event => {
+                    var model = event.dataTransfer.getData("model");
+                    if (!model) return;
+
+                    var node = new this.models[model]();
+                    var points = this.engine.getRelativeMousePoint(event);
+                    node.x = points.x;
+                    node.y = points.y;
+                    this.engine.getDiagramModel().addNode(node);
+                    this.forceUpdate();
+                }}
+                onDragOver={event => {
+                    event.preventDefault();
+                }}
+            >
+                <Tray className="tray">
+                    <TrayItem name="Component 1" model={0} />
+                </Tray>
+                <Diagram className="diagram" diagramEngine={this.engine} />
+            </div>
+        );
+    }
+}
+
+class Tray extends React.Component {
+    render() {
+        return (
+            <div {...this.props}>
+                <TrayItem name="Component 0" model={0} />
+                <TrayItem name="Component 0" model={0} />
+                <TrayItem name="Component 0" model={0} />
+                <TrayItem name="Component 0" model={0} />
+            </div>
+        );
+    }
+}
+
+class TrayItem extends React.Component {
+    render() {
+        return (
+            <div
+                draggable={true}
+                onDragStart={event => {
+                    event.dataTransfer.setData("model", this.props.model);
+                }}
+            >
+                {this.props.name}
+            </div>
         );
     }
 }
